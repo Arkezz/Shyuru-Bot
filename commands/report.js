c725c402d7392f5ae47dcc175f2ba3511ecf13cb
Arkezz <40185507+Arkezz@users.noreply.github.com>
2018-07-03 23:18:07 +0200

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -1,17 +1,19 @@
 const Discord = require("discord.js");
-const Report = require("../models/report.js");
 const botconfig = require("../botconfig.json");
-const errors = require("../utils/errors.js");
 const red = botconfig.red;
 const green = botconfig.green;
 const orange = botconfig.orange;
-
+const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => {
-    await message.delete();
-    let rUser = message.mentions.members.first();
+    message.delete();
+    if(args[0] == "help"){
+      message.reply("Usage: !report <user> <reason>");
+      return;
+    }
+    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!rUser) return errors.cantfindUser(message.channel);
-    let rreason = args.slice(1).join(" ");
+    let rreason = args.join(" ").slice(22);
     if(!rreason) return errors.noReason(message.channel);
 
     let reportEmbed = new Discord.RichEmbed()
@@ -23,16 +25,9 @@ module.exports.run = async (bot, message, args) => {
     .addField("Time", message.createdAt)
     .addField("Reason", rreason);
 
-  let reportschannel = message.guild.channels.find(c => c.name == "reports");
+    let reportschannel = message.guild.channels.find(`name`, "reports");
     if(!reportschannel) return message.channel.send("Couldn't find reports channel.");
     reportschannel.send(reportEmbed);
-
-
-
-
-
-
-
 
 }
 
