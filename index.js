09ca4a9f436f1611904f50ed82ea69305eb8726a
Arkezz <40185507+Arkezz@users.noreply.github.com>
2018-07-05 03:54:22 +0200

    update

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client();
 const token = process.env.token;
+const Canvas = require('canvas');
+const snekfetch = require('snekfetch');
 bot.commands = new Discord.Collection();
 let coins = require("./coins.json");
 let xp = require("./xp.json");
@@ -15,7 +17,7 @@ fs.readdir("./commands/", (err, files) => {
   if(err) console.log(err);
   let jsfile = files.filter(f => f.split(".").pop() === "js");
   if(jsfile.length <= 0){
-    console.log("Couldn't find commands.");
+    console.log("Couldn't find commands."); 
     return;
   }
 
@@ -30,6 +32,59 @@ bot.on("ready", async () => {
 
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
   bot.user.setActivity("Boyfriend of the dead", {type: "WATCHING"});
+
+  const applyText = (canvas, text) => {
+    const ctx = canvas.getContext('2d');
+
+    // Declare a base size of the font
+    let fontSize = 70;
+
+    do {
+        // Assign the font to the context and decrement it so it can be measured again
+        ctx.font = `${fontSize -= 10}px sans-serif`;
+        // Compare pixel width of the text to the canvas minus the approximate avatar size
+    } while (ctx.measureText(text).width > canvas.width - 300);
+
+    // Return the result to use in the actual canvas
+    return ctx.font;
+};
+
+  bot.on('guildMemberAdd', async member => {
+    const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+    if (!channel) return;
+
+    const canvas = Canvas.createCanvas(700, 250);
+    const ctx = canvas.getContext('2d');
+
+    const background = await Canvas.loadImage('./wallpaper.jpg');
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+    ctx.strokeStyle = '#74037b';
+    ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+    // Slightly smaller text placed above the member's display name
+    ctx.font = '28px sans-serif';
+    ctx.fillStyle = '#ffffff';
+    ctx.fillText('Welcome to the server,', canvas.width / 2.5, canvas.height / 3.5);
+
+    // Add an exclamation point here and below
+    ctx.font = applyText(canvas, `${member.displayName}!`);
+    ctx.fillStyle = '#ffffff';
+    ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
+
+    ctx.beginPath();
+    ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.clip();
+
+    const { body: buffer } = await snekfetch.get(member.user.displayAvatarURL);
+    const avatar = await Canvas.loadImage(buffer);
+    ctx.drawImage(avatar, 25, 25, 200, 200);
+
+    const attachment = new Discord.Attachment(canvas.toBuffer(), 'welcome-image.png');
+
+    channel.send(`Welcome to the server, ${member}!`, attachment);
+});
 
 });
 
@@ -61,7 +116,7 @@ bot.on("message", async message => {
       coins: coins[message.author.id].coins + coinAmt
     };
   fs.writeFile("./coins.json", JSON.stringify(coins), (err) => {
-    if (err) console.log(err)
+    if (err) console.log(err);
   });
   let coinEmbed = new Discord.RichEmbed()
   .setAuthor(message.author.username)
@@ -72,7 +127,7 @@ bot.on("message", async message => {
   }
 
   let xpAdd = Math.floor(Math.random() * 7) + 8;
-  
+
 
   if(!xp[message.author.id]){
     xp[message.author.id] = {
@@ -85,7 +140,7 @@ bot.on("message", async message => {
   let curxp = xp[message.author.id].xp;
   let curlvl = xp[message.author.id].level;
   let nxtLvl = xp[message.author.id].level * 300;
-  xp[message.author.id].xp =  curxp + xpAdd;
+  xp[message.author.id].xp = curxp + xpAdd;
   if(nxtLvl <= xp[message.author.id].xp){
     xp[message.author.id].level = curlvl + 1;
     let lvlup = new Discord.RichEmbed()
@@ -96,7 +151,7 @@ bot.on("message", async message => {
     message.channel.send(lvlup).then(msg => {msg.delete(5000)});
   }
   fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
-    if(err) console.log(err)
+    if(err) console.log(err);
   });
   let prefix = prefixes[message.guild.id].prefixes;
   if(!message.content.startsWith(prefix)) return;
@@ -114,11 +169,11 @@ bot.on("message", async message => {
   let args = messageArray.slice(1); 
 
   let commandfile = bot.commands.get(cmd.slice(prefix.length));
-  if(commandfile) commandfile.run(bot,message,args);
+  if(commandfile) commandfile.run(bot, message, args);
 
   setTimeout(() => {
-    cooldown.delete(message.author.id)
-  }, cdseconds * 1000)
+    cooldown.delete(message.author.id);
+  }, cdseconds * 1000);
 
 
   
