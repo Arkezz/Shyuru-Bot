37810df55a01ea2fc2f7f3b55227f8e0bcfb6eb6
Arkezz <40185507+Arkezz@users.noreply.github.com>
2018-07-06 21:02:25 +0200

    update

---
+++ commands/play.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const ytdl = require("ytdl-core");
+
+    module.exports.run = async (bot, message, args) => {
+
+    if (!message.member.voiceChannel) return message.channel.send("please connect to a voice channel.");
+
+    if (message.guild.me.voiceChannel) return message.channel.send("sorry, the bot is already connect to a voice channel");
+
+    if (!args[0]) return message.channel.send("sorry, please input a url following the command");
+
+    let validate = await ytdl.validateURL(args[0]);
+
+    if (!validate) return message.channel.send("sorry please input a **valid** url following the command.");
+
+    let info = await ytdl.getInfo(args[0]);
+
+    let connection = await message.member.voiceChannel.join();
+
+    let dispatcher = await connection.play(ytdl(args[0], { filter: 'audioonly'}));
+
+    message.channel.send(`now playing: ${info.title}`);
+
+    
+        
+    }
+
+
+
+module.exports.help = {
+    name: "play"
+  }
