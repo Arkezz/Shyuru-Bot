e7304c53aecfbfafa12b6b801a81729f71f9c90b
Arkezz <40185507+Arkezz@users.noreply.github.com>
2018-07-06 22:51:43 +0200

    update

--- commands/play.js
+++ commands/play.js
@@ -17,9 +17,10 @@ const active = new Map();
 
     let connection = await message.member.voiceChannel.join();
 
-    let dispatcher = await connection.play(ytdl(args[0], { filter: 'audioonly'}));
+    let dispatcher = await connection.playStream(ytdl(args[0], { filter: 'audioonly'}));
 
     message.channel.send(`now playing: ${info.title}`);
+
 
     
         
