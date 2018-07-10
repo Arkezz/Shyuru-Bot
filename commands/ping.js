e90fd17759c35665d604eb2bd6c05441fc235b15
Arkezz <40185507+Arkezz@users.noreply.github.com>
2018-07-10 23:19:26 +0200

    update

--- commands/ping.js
+++ commands/ping.js
@@ -1,12 +1,14 @@
-module.exports.run = async (bot, message, args) => {
+const Discord = require("discord.js");
 
+module.exports.run = async (bot, message, args) => {
+    
     let pingembed = new Discord.RichEmbed()
     .setTitle("Ping!")
     .setDescription("Pong!")
     .setColor("RANDOM")
-    .addField("Ping:", `${Date.now() - message.createdTimestamp}` + ' ms');
-    return message.channel.send(pingembed)
-    message.delete().catch(O_o=>{});
+    .addField("Ping: ", `${message.createdTimestamp - Date.now()}` + ' ms');
+    return message.channel.send(pingembed);
+    
 }
     
   
