e78406cf84b675401e42119a756a989d2b75853f
Arkezz <40185507+Arkezz@users.noreply.github.com>
2018-07-10 18:09:00 +0200

    update

---
+++ commands/messageslb.js
@@ -0,0 +1,22 @@
+const db = require("quick.db");
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+db.startsWith(`guildMessages_${message.guild.id}`, { sort: `data` }).then(resp => {
+
+    resp.length = 15;
+
+    let finalOutput = `**Leaderboard!:**\n\n`;
+    for(var i in resp) {
+        finalOutput += `${bot.users.get(resp[i].ID.split('_')[2]).username} -- ${resp[i].data} messages\n`;
+    }
+
+
+message.channel.send(finalOutput);
+});
+
+}
+
+module.exports.help = {
+    name: "messageslb"
+  }
