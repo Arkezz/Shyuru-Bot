ae8016505b24fd97c39530c4e88b7b8b4818cd26
Arkezz <40185507+Arkezz@users.noreply.github.com>
2018-07-12 00:27:14 +0200

    Update dog.js

--- commands/dog.js
+++ commands/dog.js
@@ -3,7 +3,7 @@ const superagent = require("superagent")
 
 module.exports.run = async (bot, message, args) => {
     const { body } = await superagent
-    .get('https://random.dog/woof.json');
+    .get('https://dog.ceo/api/breeds/image/random');
     const embed = new Discord.RichEmbed()
     .setColor(0x954D23)
     .setTitle("Bark :dog:")
